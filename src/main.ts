9f8bb0b2325f20268de14706a7254b86c43170fa
elviritica <e.aviles.s@gmail.com>
2024-03-16 11:00:37 +0100

    Caso 1 y 2 terminados

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,96 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+const __STANDARD = 100;
+const __SUITE = 150;
+const __HABTOUROP = 100;
+const __PAXEXTRA = 40;
+const __IVA = 21;
+const __DESCUENTOTOUROP = 15;
+
+interface Reserva {
+  tipoHabitacion: "standard" | "suite";
+  pax: number;
+  noches: number;
+}
+
+const reservas : Reserva[] = [
+  {
+    tipoHabitacion: "standard",
+    pax: 1,
+    noches: 3,
+  },
+  {
+    tipoHabitacion: "standard",
+    pax: 1,
+    noches: 4,
+  },
+  {
+    tipoHabitacion: "suite",
+    pax: 2,
+    noches: 1,
+  },
+];
+
+class CalcularReservas {
+    reservas : Reserva[];
+
+    constructor(reservas: Reserva[]) {
+        this.reservas = reservas;
+    }
+
+    calculaSubtotal(){
+        let subtotal = 0;
+        this.reservas.forEach((reserva) => {
+            if (reserva.tipoHabitacion === "standard") {
+                subtotal += __STANDARD * reserva.noches;
+            } else if (reserva.tipoHabitacion === "suite") {
+                subtotal += __SUITE * reserva.noches;
+            }
+            if (reserva.pax > 1) {
+                subtotal += __PAXEXTRA * (reserva.pax - 1) * reserva.noches;
+            }
+        });
+        return subtotal;
+    }
+
+    calculaTotal(){
+        let subtotal = this.calculaSubtotal();
+        let total = subtotal + (subtotal * __IVA)/ 100;
+
+        return total;
+    }
+};
+
+const calculadora1 = new CalcularReservas(reservas);
+console.log("Subtotal Reservas: ",calculadora1.calculaSubtotal());
+console.log("Total Reservas: ",calculadora1.calculaTotal());
+
+class CalcularReservasTourOperador extends CalcularReservas {
+    calculaSubtotal() : number{
+        let subtotal = 0;
+
+        this.reservas.forEach((reserva) => {
+            subtotal = subtotal + (reserva.pax * __HABTOUROP) * reserva.noches;
+
+            if (reserva.pax > 1) {
+                subtotal += __PAXEXTRA * (reserva.pax - 1) * reserva.noches;
+            }
+        });
+
+        return subtotal;
+    }
+
+    calculaTotal(): number {
+        let total = super.calculaTotal();
+        let totalDescuento = total - (total * __DESCUENTOTOUROP) / 100;
+    
+        return totalDescuento;
+        
+    }
+    
+}
+
+const calculadora2 = new CalcularReservasTourOperador(reservas);
+console.log("Subtotal Reservas Tour Operador: ",calculadora2.calculaSubtotal());
+console.log("Total Reservas Tour Operador: ",calculadora2.calculaTotal());
+
