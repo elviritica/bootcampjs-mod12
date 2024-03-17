2736e399719ebca2cb00139750105fdcbe61e7de
elviritica <e.aviles.s@gmail.com>
2024-03-17 14:24:01 +0100

    terminado

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Bootcamp JS - Laboratorio Módulo 12
 
-- Clonate el proyecto.
+- Clónate el proyecto.
 - Instala las dependencias con `npm install`.
 - Ejecuta el sandbox con `npm run dev`.
 - Abre el navegador en `http://localhost:5173/` (si ese puerto no te funciona, mira en la consola donde has hecho el build, puede que este ocupado y se haya abierto en otro puerto).
 
 # Laboratorio - Reservas de Hotel
 
-Este proyecto se trata de un sistema de reservas de hotel donde los clientes pueden especificar qué habitaciones desean reservar, así como la cantidad de personas que ocuparán cada habitación y la duración de su estancia.
+Este proyecto es un sistema de reservas de hotel donde los clientes pueden especificar qué habitaciones desean reservar, así como la cantidad de personas que ocuparán cada habitación y la duración de su estancia.
 
 ## Funcionalidades
 
@@ -25,10 +25,9 @@ Este proyecto se trata de un sistema de reservas de hotel donde los clientes pue
 - Todas las habitaciones tienen el mismo precio: 100 €.
 - Se aplica un 15% de descuento a los servicios contratados por el tour operador.
 
-----
 
 Se ha implementado una clase base con la funcionalidad común y dos clases hijas para cubrir los casos de cliente particular y tour operador.
 
-Se ha añadido un campo a cada reserva para indicar si el desayuno está incluido o no. En caso de estar incluido, se aplicará un cargo adicional de 15 € por persona y noche.
+Se ha añadido un campo a cada reserva para indicar si el **desayuno** está incluido o no. En caso de estar incluido, se aplicará un cargo adicional de 15 € por persona y noche.
 
 Se calculan totales y subtotales tanto para la tarifa particular como para el tour operador, teniendo en cuenta el precio de las habitaciones y los cargos adicionales.
--- src/main.ts
+++ src/main.ts
@@ -6,91 +6,125 @@ const __HABTOUROP = 100;
 const __PAXEXTRA = 40;
 const __IVA = 21;
 const __DESCUENTOTOUROP = 15;
+const __DESAYUNO = 15;
 
 interface Reserva {
   tipoHabitacion: "standard" | "suite";
+  desayuno: boolean;
   pax: number;
   noches: number;
 }
 
-const reservas : Reserva[] = [
+const reservas: Reserva[] = [
   {
     tipoHabitacion: "standard",
+    desayuno: false,
     pax: 1,
     noches: 3,
   },
   {
     tipoHabitacion: "standard",
+    desayuno: false,
     pax: 1,
     noches: 4,
   },
   {
     tipoHabitacion: "suite",
+    desayuno: true,
     pax: 2,
     noches: 1,
   },
 ];
 
 class CalcularReservas {
-    reservas : Reserva[];
+  reservas: Reserva[];
+  precios: {
+    standard: number;
+    suite: number;
+  };
 
-    constructor(reservas: Reserva[]) {
-        this.reservas = reservas;
-    }
+  constructor(
+    reservas: Reserva[],
+    precios: { standard: number; suite: number }
+  ) {
+    this.reservas = reservas;
+    this.precios = precios;
+  }
 
-    calculaSubtotal(){
-        let subtotal = 0;
-        this.reservas.forEach((reserva) => {
-            if (reserva.tipoHabitacion === "standard") {
-                subtotal += __STANDARD * reserva.noches;
-            } else if (reserva.tipoHabitacion === "suite") {
-                subtotal += __SUITE * reserva.noches;
-            }
-            if (reserva.pax > 1) {
-                subtotal += __PAXEXTRA * (reserva.pax - 1) * reserva.noches;
-            }
-        });
-        return subtotal;
-    }
+  calculaSubtotal() {
+    let subtotal = 0;
+    this.reservas.forEach((reserva) => {
+      if (reserva.tipoHabitacion === "standard") {
+        subtotal += this.precios.standard * reserva.noches;
+      } else if (reserva.tipoHabitacion === "suite") {
+        subtotal += this.precios.suite * reserva.noches;
+      }
+      if (reserva.pax > 1) {
+        subtotal += __PAXEXTRA * (reserva.pax - 1) * reserva.noches;
+      }
+      if (reserva.desayuno) {
+        subtotal += __DESAYUNO * reserva.noches * reserva.pax;
+      }
+    });
+    return subtotal;
+  }
 
-    calculaTotal(){
-        let subtotal = this.calculaSubtotal();
-        let total = subtotal + (subtotal * __IVA)/ 100;
+  calculaTotal() {
+    let subtotal = this.calculaSubtotal();
+    let total = subtotal + (subtotal * __IVA) / 100;
 
-        return total;
-    }
+    return total;
+  }
+}
+
+const preciosHabitaciones = {
+  standard: __STANDARD,
+  suite: __SUITE,
 };
 
-const calculadora1 = new CalcularReservas(reservas);
-console.log("Subtotal Reservas: ",calculadora1.calculaSubtotal());
-console.log("Total Reservas: ",calculadora1.calculaTotal());
+const calculadora1 = new CalcularReservas(reservas, preciosHabitaciones);
+console.log("Subtotal Reservas: ", calculadora1.calculaSubtotal());
+console.log("Total Reservas: ", calculadora1.calculaTotal());
+
+class CalcularReservasClienteParticular extends CalcularReservas {
+  constructor(reservas: Reserva[]) {
+    const precios = {
+      standard: 100,
+      suite: 150,
+    };
+
+    super(reservas, precios);
+  }
+}
 
 class CalcularReservasTourOperador extends CalcularReservas {
-    calculaSubtotal() : number{
-        let subtotal = 0;
+  constructor(reservas: Reserva[]) {
+    const precios = {
+      standard: __HABTOUROP,
+      suite: __HABTOUROP,
+    };
 
-        this.reservas.forEach((reserva) => {
-            subtotal = subtotal + (reserva.pax * __HABTOUROP) * reserva.noches;
+    super(reservas, precios);
+  }
 
-            if (reserva.pax > 1) {
-                subtotal += __PAXEXTRA * (reserva.pax - 1) * reserva.noches;
-            }
-        });
+  calculaTotal(): number {
+    let total = super.calculaTotal();
+    let totalDescuento = total - (total * __DESCUENTOTOUROP) / 100;
 
-        return subtotal;
-    }
-
-    calculaTotal(): number {
-        let total = super.calculaTotal();
-        let totalDescuento = total - (total * __DESCUENTOTOUROP) / 100;
-    
-        return totalDescuento;
-        
-    }
-    
+    return totalDescuento;
+  }
 }
 
 const calculadora2 = new CalcularReservasTourOperador(reservas);
-console.log("Subtotal Reservas Tour Operador: ",calculadora2.calculaSubtotal());
-console.log("Total Reservas Tour Operador: ",calculadora2.calculaTotal());
+console.log(
+  "Subtotal Reservas Tour Operador: ",
+  calculadora2.calculaSubtotal()
+);
+console.log("Total Reservas Tour Operador: ", calculadora2.calculaTotal());
 
+const calculadora3 = new CalcularReservasClienteParticular(reservas);
+console.log(
+  "Subtotal Reservas Cliente Particular: ",
+  calculadora3.calculaSubtotal()
+);
+console.log("Total Reservas Cliente Particular: ", calculadora3.calculaTotal());
